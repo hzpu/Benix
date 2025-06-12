3a3b404f9609906e5dce59e1ec9fec46ebb764f0
hzpu <zhdhd77@proton.me>
2025-06-12 18:55:22 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,24 @@
+-- legends of speed
 if game.PlaceId == 3101667897 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/hzpu/Benix/refs/heads/main/LOS",true))()
+end
+
+-- grow a garden
+if game.PlaceId == 126884695634066 then
+  loadstring(game:HttpGet("https://pastebin.com/raw/kj6J3uwX"))()
+end
+
+-- Obby But You’re On a Bike
+if game.PlaceId == 14184086618 then
+  loadstring(game:HttpGet("https://pastebin.com/raw/kj6J3uwX"))()
+end
+
+-- Dig To Earth Core
+if game.PlaceId == 81440632616906 then
+  loadstring(game:HttpGet("https://pastebin.com/raw/Z0WEqm4a"))()
+end
+
+-- Wall Hop Practice
+if game.PlaceId == 5044920251 then
+  loadstring(game:HttpGet("https://pastebin.com/raw/Sq9sWsNY"))()
 end
