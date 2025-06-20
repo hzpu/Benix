061c630bd7cc8e4d858dc2c6c9197fa7eff1896f
hzpu <zhdhd77@proton.me>
2025-06-20 13:22:50 -0400

    Update mm2publicbeta.lua

--- mm2publicbeta.lua
+++ mm2publicbeta.lua
@@ -41,14 +41,13 @@ game:GetService("RunService").Heartbeat:Connect(function()
 end)
 
 if _G.antiafk then
-    while true do
     game:GetService("Players").LocalPlayer.Idled:Connect(function()
-        vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        vu:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
         task.wait(1)
-        vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-    end
+        vu:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
     end)
 end
+
 if _G.fpslimit then
     setfpscap(_G.fpslimit)
 end
